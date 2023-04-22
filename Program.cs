e1bff6c6f92b91a8c1350d9114021379868c7fe6
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-21 21:54:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double quilometros;
+            double mph;
+            double conv = 1852;
+
+            Console.WriteLine("valor de milhas");
+            mph = double.Parse(Console.ReadLine());
+
+            quilometros = mph * conv;
+            Console.WriteLine("valor da conversao {0}", quilometros);
+        }
+    }
+}
